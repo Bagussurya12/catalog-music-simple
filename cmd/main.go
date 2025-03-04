e3bd4d62eba6e31ca62b1b597334c9288f4249d1
Bagussurya12 <bagus.suryaindrajati@gmail.com>
2025-03-04 22:24:32 +0700

    add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Bagussurya12/catalog-music-simple/pkg/internalsql"
+	"github.com/Bagussurya12/catalog-music-simple/source/configs"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder(
+			[]string{"./source/configs/"},
+		),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+
+	if err != nil {
+		log.Fatal("Failed Configuration:", err)
+	}
+
+	cfg = configs.Get()
+
+	db, err := internalsql.Connect(cfg.Database.DataSourceName)
+	if err != nil {
+		log.Fatalf("failed connect to database, err: %+v", err)
+	}
+
+	r := gin.Default()
+	r.Run(cfg.Service.Port)
 }
